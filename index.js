cc9f3c62eb3dc7fab6c5314360cda7394d1ad692
Utkarsh-Jain2199 <77992826+Utkarsh-Jain2199@users.noreply.github.com>
2024-08-20 20:20:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 
-global.foodData = require('./db')(function call(err, data, CatData) {
-  // console.log(data)
-  if(err) console.log(err);
+// global.foodData = require('./db')(function call(err, data, CatData) {
+//   // console.log(data)
+//   if(err) console.log(err);
+//   global.foodData = data;
+//   global.foodCategory = CatData;
+// })
+
+// const express = require('express')
+// const app = express()
+// const port = 5000
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+// app.use(express.json())
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.use('/api/auth', require('./Routes/Auth'));
+
+// app.listen(process.env.PORT, () => {
+// console.log(`Example app listening on http://localhost:${port}`)
+// })
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Set up the database and global variables
+const db = require('./db');
+db((err, data, CatData) => {
+  if (err) {
+    console.error('Database connection error:', err);
+    return;
+  }
   global.foodData = data;
   global.foodCategory = CatData;
-})
+  console.log('Global data initialized.');
+});
 
-const express = require('express')
-const app = express()
-const port = 5000
+// Middleware to handle CORS
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
@@ -17,15 +54,19 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(express.json())
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.use('/api/auth', require('./Routes/Auth'));
 
-app.listen(process.env.PORT, () => {
-console.log(`Example app listening on http://localhost:${port}`)
-})
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
+});
 
